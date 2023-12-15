982cc439f0f9a4ca4043605de11bc946f0b8f08b
Ancfrank <45130128+JSnack58@users.noreply.github.com>
2023-12-15 10:58:29 -0800

    Delete microservices-parent/product-service directory
    "properties fix2"

--- microservices-parent/product-service/README.md
+++
@@ -1,8 +0,0 @@
-Model: Holds the class that contains the data to be stored in the database.
-
-Controller: Creates the REST endpoints to be accessed by the client to reach the service.
-
-Service: Creates the service that handles the incoming data from the client.
-
-Repository: The interface the service uses to get the data into MongoDB.
-
--- microservices-parent/product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.productservice.repository;
-
-import com.example.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-// When creating an Interface repo should have the <model class, model's id>
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
